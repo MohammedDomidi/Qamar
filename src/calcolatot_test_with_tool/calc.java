d4cb8efb08db03b9a98a31b8ad493c2873533d83
MohammedDomidi <ironmanmoedom@gmail.com>
2020-03-02 19:40:31 +0200

    1st

---
+++ src/calcolatot_test_with_tool/calc.java
@@ -0,0 +1,28 @@
+package calcolatot_test_with_tool;
+
+
+public class calc {
+	
+		public int add(int a, int b )	
+		{
+			return a+b;
+		}
+	
+		public int sub(int a, int b )	
+		{
+			return a-b;
+		}
+	
+		public int mult(int a, int b )	
+		{
+			return a*b;
+		}
+	
+		public int div(int a, int b )	
+		{
+			return a/b;
+		}
+	
+		
+	
+}
