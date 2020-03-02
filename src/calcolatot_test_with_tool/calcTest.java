700b9e4d0d48cf4a4f957649ebbc9710de45d871
MohammedDomidi <ironmanmoedom@gmail.com>
2020-03-02 19:44:00 +0200

    ok

---
+++ src/calcolatot_test_with_tool/calcTest.java
@@ -0,0 +1,19 @@
+package calcolatot_test_with_tool;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class calcTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
